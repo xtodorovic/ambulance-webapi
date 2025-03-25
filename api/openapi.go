e303138a9ff28f93b263feef97c0134d141172b5
xtodorovic <mtodorovic81@gmail.com>
2025-03-25 15:30:23 +0100

    added config for go and openapi

--- api/openapi.go
+++ api/openapi.go
@@ -1,15 +1,15 @@
 package api
 
 import (
-    _ "embed"
-    "net/http"
+	_ "embed"
+	"net/http"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 //go:embed ambulance-wl.openapi.yaml
 var openapiSpec []byte
 
 func HandleOpenApi(ctx *gin.Context) {
-    ctx.Data(http.StatusOK, "application/yaml", openapiSpec)
+	ctx.Data(http.StatusOK, "application/yaml", openapiSpec)
 }
--- cmd/ambulance-api-service/main.go
+++ cmd/ambulance-api-service/main.go
@@ -1,26 +1,33 @@
 package main
 
 import (
-    "log"
-    "os"
-    "strings"
-    "github.com/gin-gonic/gin"
-    "github.com/xtodorovic/ambulance-webapi/api"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/xtodorovic/ambulance-webapi.git/api"
+	"github.com/xtodorovic/ambulance-webapi/internal/ambulance_wl"
 )
 
 func main() {
-    log.Printf("Server started")
-    port := os.Getenv("AMBULANCE_API_PORT")
-    if port == "" {
-        port = "8080"
-    }
-    environment := os.Getenv("AMBULANCE_API_ENVIRONMENT")
-    if !strings.EqualFold(environment, "production") { // case insensitive comparison
-        gin.SetMode(gin.DebugMode)
-    }
-    engine := gin.New()
-    engine.Use(gin.Recovery())
-    // request routings
-    engine.GET("/openapi", api.HandleOpenApi)
-    engine.Run(":" + port)
+	log.Printf("Server started")
+	port := os.Getenv("AMBULANCE_API_PORT")
+	if port == "" {
+		port = "8080"
+	}
+	environment := os.Getenv("AMBULANCE_API_ENVIRONMENT")
+	if !strings.EqualFold(environment, "production") { // case insensitive comparison
+		gin.SetMode(gin.DebugMode)
+	}
+	engine := gin.New()
+	engine.Use(gin.Recovery())
+	// request routings
+	handleFunctions := &ambulance_wl.ApiHandleFunctions{
+		AmbulanceConditionsAPI:  ambulance_wl.NewAmbulanceConditionsApi(),
+		AmbulanceWaitingListAPI: ambulance_wl.NewAmbulanceWaitingListApi(),
+	}
+	ambulance_wl.NewRouterWithGinEngine(engine, *handleFunctions)
+	engine.GET("/openapi", api.HandleOpenApi)
+	engine.Run(":" + port)
 }
