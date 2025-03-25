d45dba525f40131a56d5869048897eefc1e7f7d7
xtodorovic <mtodorovic81@gmail.com>
2025-03-25 23:57:58 +0100

    ambulance waiting list

--- cmd/ambulance-api-service/main.go
+++ cmd/ambulance-api-service/main.go
@@ -8,6 +8,10 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/xtodorovic/ambulance-webapi.git/api"
 	"github.com/xtodorovic/ambulance-webapi.git/internal/ambulance_wl"
+	"github.com/xtodorovic/ambulance-webapi.git/internal/db_service"
+    "context"
+    "time"
+    "github.com/gin-contrib/cors"
 )
 
 func main() {
@@ -22,10 +26,28 @@ func main() {
 	}
 	engine := gin.New()
 	engine.Use(gin.Recovery())
+	corsMiddleware := cors.New(cors.Config{
+        AllowOrigins:     []string{"*"},
+        AllowMethods:     []string{"GET", "PUT", "POST", "DELETE", "PATCH"},
+        AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+        ExposeHeaders:    []string{""},
+        AllowCredentials: false,
+        MaxAge: 12 * time.Hour,
+    })
+    engine.Use(corsMiddleware)
+
+	// setup context update  middleware
+	dbService := db_service.NewMongoService[ambulance_wl.Ambulance](db_service.MongoServiceConfig{})
+	defer dbService.Disconnect(context.Background())
+	engine.Use(func(ctx *gin.Context) {
+		ctx.Set("db_service", dbService)
+		ctx.Next()
+	})
 	// request routings
 	handleFunctions := &ambulance_wl.ApiHandleFunctions{
 		AmbulanceConditionsAPI:  ambulance_wl.NewAmbulanceConditionsApi(),
 		AmbulanceWaitingListAPI: ambulance_wl.NewAmbulanceWaitingListApi(),
+		AmbulancesAPI:           ambulance_wl.NewAmbulancesApi(),
 	}
 	ambulance_wl.NewRouterWithGinEngine(engine, *handleFunctions)
 	engine.GET("/openapi", api.HandleOpenApi)
--- internal/ambulance_wl/README.md
+++ internal/ambulance_wl/README.md
@@ -13,7 +13,7 @@ To see how to make this your own, look here:
 [README](https://openapi-generator.tech)
 
 - API version: 1.0.0
-- Build date: 2025-03-25T12:45:55.845580139Z[Etc/UTC]
+- Build date: 2025-03-25T22:22:16.606949747Z[Etc/UTC]
 - Generator version: 7.13.0-SNAPSHOT
 
 ### Running the server
